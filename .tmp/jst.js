29dd7d0767871de9c5b5f38832fce9ca6d772aba
Max Degterev <me@maxdegterev.name>
2014-05-19 14:24:04 +0200

    awesome stuff

--- .tmp/jst.js
+++ .tmp/jst.js
@@ -1,15 +1,17 @@
-var jade = jade || require('jade').runtime;
-
-this["JST"] = this["JST"] || {};
-
-this["JST"]["sample_template"] = function template(locals) {
+function sample_template(locals) {
 var buf = [];
 var jade_mixins = {};
 var jade_interp;
-var locals_ = (locals || {}),env = locals_.env;
+;var locals_for_with = (locals || {});(function (JSON, env) {
 var jade_indent = [];
 var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-buf.push("\n<h2>JS templating also works!</h2><span>Helpers: " + (jade.escape((jade_interp = jade.helpers.shorten(lorem, 20)) == null ? '' : jade_interp)) + "</span>\n<pre>" + (jade.escape(null == (jade_interp = JSON.stringify(env, null, 2)) ? "" : jade_interp)) + "</pre>");;return buf.join("");
-};
+buf.push("\n<h2>JS templating also works!</h2><span>Helpers: " + (jade.escape((jade_interp = jade.helpers.shorten(lorem, 20)) == null ? '' : jade_interp)) + "</span>\n<pre>" + (jade.escape(null == (jade_interp = JSON.stringify(env, null, 2)) ? "" : jade_interp)) + "</pre>");}("JSON" in locals_for_with?locals_for_with.JSON:typeof JSON!=="undefined"?JSON:undefined,"env" in locals_for_with?locals_for_with.env:typeof env!=="undefined"?env:undefined));;return buf.join("");
+}
+function blocks/awesome(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
 
-if (typeof exports === 'object' && exports) {module.exports = this["JST"];}
+var jade_indent = [];
+buf.push("\n<p>wow template</p>");;return buf.join("");
+}
--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,18 +1,32 @@
 pkg = require('./package')
 
 gulp = require('gulp')
-source = require('vinyl-source-stream')
-rename = require('gulp-rename')
 
 browserify = require('browserify')
+watchify = require('watchify')
+source = require('vinyl-source-stream')
+
 stylus = require('gulp-stylus')
+rename = require('gulp-rename')
 nib = require('nib')
 
+jade = require('gulp-jade')
+commonjs = require('gulp-commonjs-jade')
+concat = require('gulp-concat')
+
+watch = false
+
 gulp.task 'browserify', ->
-  browserify('./app/javascripts/client/index.coffee', extensions: ['.coffee'])
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./public/assets'))
+  options = ['./app/javascripts/client/index.coffee', extensions: ['.coffee']]
+  bundler = if watch then watchify(options...) else browserify(options...)
+
+  compile = ->
+    bundler.bundle()
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('./public/assets'))
+
+  bundler.on('update', compile)
+  compile()
 
 gulp.task 'stylus', ->
   gulp.src('./app/stylesheets/index.styl')
@@ -27,6 +41,19 @@ gulp.task 'stylus', ->
     ))
     .pipe(rename('app.css'))
     .pipe(gulp.dest('./public/assets'))
+
+gulp.task 'jade', ->
+  gulp.src('./app/templates/client/**/*.jade')
+    .pipe(jade(
+      pretty: true
+      compileDebug: false
+      client: true
+    ))
+    .pipe(commonjs(
+      processName: (file)-> file.replace(/^.*app\/templates\/client\/([\w\/]+).js$/gi, '$1')
+    ))
+    .pipe(concat('jst.js'))
+    .pipe(gulp.dest('./.tmp'))
 
 gulp.task 'default', ['browserify', 'stylus'], ->
 gulp.task 'build', ['default'], ->
