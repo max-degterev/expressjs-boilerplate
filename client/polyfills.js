ddefdd87a60c487f5e136a077df3c78afa2e7033
Max Degterev <max@degterev.me>
2016-11-04 18:35:53 +0100

    minimal polyfill

--- client/polyfills.js
+++
@@ -1,6 +0,0 @@
-require('core-js/es6/object'); // Used by Babel
-require('core-js/es6/array'); // Used by Babel
-require('core-js/es6/string');
-require('core-js/es6/number');
-require('core-js/es6/promise');
-require('core-js/es6/symbol'); // Used by Babel
