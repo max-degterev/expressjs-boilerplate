db82807267a8a346068f994cb589ab5963c1896e
Max Degterev <max@degterev.me>
2019-09-25 15:47:59 +0200

    POC WIP

--- build/styles.js
+++ build/styles.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 
 const config = require('uni-config');
+const sass = require('gulp-sass');
 const utils = require('./utils');
 
-const source = `${__dirname}/../styles/index.styl`;
+const stylusSource = `${__dirname}/../styles/index.styl`;
+const sassSource = `${__dirname}/../styles/index.scss`;
+
 
 const stylusOptions = {
   errors: config.debug,
@@ -20,13 +23,25 @@ const stylusOptions = {
   },
 };
 
-const process = (options = {}) => {
-  const startTime = Date.now();
-  const name = 'app.css';
+const sassOptions = {
+  includePaths: [
+    `${__dirname}/../client`,
+    `${__dirname}/../node_modules`,
+  ],
+  importer: require('node-sass-glob-importer')(),
+  outputStyle: 'expanded',
+  sourceComments: config.debug,
+};
 
-  const executor = (resolve) => {
+sass.compiler = require('node-sass');
+
+const process = (options = {}) => {
+  const stylusPromise = new Promise((resolve) => {
+    const startTime = Date.now();
+    const name = 'app-stylus.css';
+
     const stream = gulp
-      .src(source)
+      .src(stylusSource)
       .pipe(require('gulp-stylus')(stylusOptions))
       .pipe(require('gulp-postcss')([
         require('autoprefixer')(),
@@ -38,14 +53,39 @@ const process = (options = {}) => {
       .pipe(gulp.dest(`${__dirname}/../${config.build.assets_location}`))
 
       .on('end', () => {
-        utils.benchmarkReporter(`Stylusified ${utils.pathNormalize(source)}`, startTime);
+        utils.benchmarkReporter(`Stylusified ${utils.pathNormalize(stylusSource)}`, startTime);
         resolve(name);
       });
 
     if (options.pipe) options.pipe(stream);
-  };
+  });
 
-  return new Promise(executor);
+  const sassPromise = new Promise((resolve) => {
+    const startTime = Date.now();
+    const name = 'app-sass.css';
+
+    const stream = gulp
+      .src(sassSource)
+      // .pipe(require('gulp-sass-glob')())
+      .pipe(sass(sassOptions))
+      .pipe(require('gulp-postcss')([
+        require('autoprefixer')(),
+        require('postcss-flexbugs-fixes'),
+      ]))
+      .on('error', utils.errorReporter)
+
+      .pipe(require('gulp-rename')(name))
+      .pipe(gulp.dest(`${__dirname}/../${config.build.assets_location}`))
+
+      .on('end', () => {
+        utils.benchmarkReporter(`Sassified ${utils.pathNormalize(sassSource)}`, startTime);
+        resolve(name);
+      });
+
+    if (options.pipe) options.pipe(stream);
+  });
+
+  return Promise.all([stylusPromise, sassPromise]);
 };
 
 module.exports = process;
