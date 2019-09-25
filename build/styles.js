4a59a0a2b78d45f83f0baca471a0b616620e9254
Max Degterev <max@degterev.me>
2019-09-25 17:13:12 +0200

    all mixins converted, still need testing

--- build/styles.js
+++ build/styles.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 
 const config = require('uni-config');
-const sass = require('gulp-sass');
+const sass = require('node-sass');
+const gulpSass = require('gulp-sass');
+const sizeOf = require('image-size');
 const utils = require('./utils');
 
 const stylusSource = `${__dirname}/../styles/index.styl`;
 const sassSource = `${__dirname}/../styles/index.scss`;
+const publicRoot = `${__dirname}/../${config.build.public_root}`;
 
 
 const stylusOptions = {
@@ -19,7 +22,29 @@ const stylusOptions = {
   urlfunc: 'embedurl',
   linenos: config.debug,
   rawDefine: {
-    $publicRoot: `${__dirname}/../${config.build.public_root}`,
+    $publicRoot: publicRoot,
+  },
+};
+
+const functions = {
+  'replace($pattern, $string, $replacement)': (pattern, string, replacement) => {
+    const regex = new RegExp(pattern.getValue(), 'g');
+    const result = string.getValue().replace(regex, replacement.getValue());
+    return new sass.types.String(result);
+  },
+  'image-size($url)': (path) => {
+    const file = `${publicRoot}/${path.getValue()}`;
+
+    const dimensions = sizeOf(file);
+    const keys = ['width', 'height'];
+    const map = new sass.types.Map(keys.length);
+
+    keys.forEach((key, index) => {
+      map.setKey(index, new sass.types.String(key));
+      map.setValue(index, new sass.types.Number(dimensions[key], 'px'));
+    });
+
+    return map;
   },
 };
 
@@ -28,12 +53,14 @@ const sassOptions = {
     `${__dirname}/../client`,
     `${__dirname}/../node_modules`,
   ],
+  functions,
   importer: require('node-sass-glob-importer')(),
   outputStyle: 'expanded',
   sourceComments: config.debug,
 };
 
-sass.compiler = require('node-sass');
+gulpSass.compiler = sass;
+
 
 const process = (options = {}) => {
   const stylusPromise = new Promise((resolve) => {
@@ -66,8 +93,7 @@ const process = (options = {}) => {
 
     const stream = gulp
       .src(sassSource)
-      // .pipe(require('gulp-sass-glob')())
-      .pipe(sass(sassOptions))
+      .pipe(gulpSass(sassOptions))
       .pipe(require('gulp-postcss')([
         require('autoprefixer')(),
         require('postcss-flexbugs-fixes'),
