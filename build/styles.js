1e15ce5f09836d635689d7dadc23c7059e5c42ca
Max Degterev <max@degterev.me>
2019-09-30 17:18:57 +0200

    clean up sass compiler

--- build/styles.js
+++ build/styles.js
@@ -3,13 +3,11 @@ const gulp = require('gulp');
 const config = require('uni-config');
 const sass = require('node-sass');
 const gulpSass = require('gulp-sass');
-const sizeOf = require('image-size');
 const utils = require('./utils');
 
 const stylusSource = `${__dirname}/../styles/index.styl`;
 const sassSource = `${__dirname}/../styles/index.scss`;
 const publicRoot = `${__dirname}/../${config.build.public_root}`;
-
 
 const stylusOptions = {
   errors: config.debug,
@@ -26,34 +24,12 @@ const stylusOptions = {
   },
 };
 
-const functions = {
-  'replace($pattern, $string, $replacement)': (pattern, string, replacement) => {
-    const regex = new RegExp(pattern.getValue(), 'g');
-    const result = string.getValue().replace(regex, replacement.getValue());
-    return new sass.types.String(result);
-  },
-  'image-size($url)': (path) => {
-    const file = `${publicRoot}/${path.getValue()}`;
-
-    const dimensions = sizeOf(file);
-    const keys = ['width', 'height'];
-    const map = new sass.types.Map(keys.length);
-
-    keys.forEach((key, index) => {
-      map.setKey(index, new sass.types.String(key));
-      map.setValue(index, new sass.types.Number(dimensions[key], 'px'));
-    });
-
-    return map;
-  },
-};
-
 const sassOptions = {
   includePaths: [
     `${__dirname}/../client`,
     `${__dirname}/../node_modules`,
   ],
-  functions,
+  functions: require('sass-functions')({ publicRoot }),
   importer: require('node-sass-glob-importer')(),
   outputStyle: 'expanded',
   sourceComments: config.debug,
